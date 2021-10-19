2397a58d98769940fa2371ca3dc94ffd77861de0
Edward259 <yjw@onyx-international.com>
2021-10-19 17:41:48 +0800

    feat:add PasswordEditText view

--- app/src/main/java/com/edward/edwardapplication/CustomViewActivity.java
+++ app/src/main/java/com/edward/edwardapplication/CustomViewActivity.java
@@ -6,13 +6,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import com.edward.edwardapplication.databinding.ActivityCustomViewBinding;
 import com.edward.edwardapplication.view.MyStatusCheckBox;
+import com.edward.edwardapplication.view.PasswordEditText;
 
 public class CustomViewActivity extends AppCompatActivity {
 
@@ -33,9 +37,7 @@ public class CustomViewActivity extends AppCompatActivity {
             }
         };
         registerReceiver(mReceiver, mIntentFilter);
-
         unregisterReceiver(mReceiver);
-
     }
 
     private void handleStateChanged(int state) {
@@ -71,6 +73,71 @@ public class CustomViewActivity extends AppCompatActivity {
                 }
             }
         });
+
+        binding.etPassword.setOnTextChangeListener(new PasswordEditText.OnTextChangeListener() {
+            @Override
+            public void onTextChange(String pwd) {
+                if (pwd.length() == binding.etPassword.getTextLength()) {
+                    //输入监听
+                    Toast.makeText(CustomViewActivity.this, pwd, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        binding.etStar.setOnTextChangeListener(new PasswordEditText.OnTextChangeListener() {
+            @Override
+            public void onTextChange(String pwd) {
+                if (pwd.length() == binding.etStar.getTextLength()) {
+                    //输入监听
+                    Toast.makeText(CustomViewActivity.this, pwd, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        PasswordEditText currentFocusEditText = (PasswordEditText) getCurrentFocusEditText();
+        binding.clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PasswordEditText currentFocusEditText = (PasswordEditText) getCurrentFocusEditText();
+                if (currentFocusEditText == null) {
+                    return;
+                }
+                currentFocusEditText.clearText(); //清空输入内容
+            }
+        });
+        binding.add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PasswordEditText currentFocusEditText = (PasswordEditText) getCurrentFocusEditText();
+                if (currentFocusEditText == null) {
+                    return;
+                }
+                currentFocusEditText.addChar("2");
+            }
+        });
+        binding.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PasswordEditText currentFocusEditText = (PasswordEditText) getCurrentFocusEditText();
+                if (currentFocusEditText == null) {
+                    return;
+                }
+                currentFocusEditText.deleteChar();
+            }
+        });
+    }
+
+    private EditText getCurrentFocusEditText() {
+        if (getCurrentFocus() == null) {
+            return null;
+        }
+        switch (getCurrentFocus().getId()) {
+            case R.id.et_password:
+                return binding.etPassword;
+            case R.id.et_star:
+                return binding.etStar;
+            default:
+                return null;
+        }
     }
 
     private BluetoothAdapter getBluetoothAdapter() {
